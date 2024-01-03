bf6fe1468d67b0eb71e055f4b442337f4da1a57f
Satoshi TAKEDA <satoshi.takeda@classi.jp>
2024-01-03 15:29:30 +0900

    chore: reafactor main_test

--- main_test.go
+++ main_test.go
@@ -11,20 +11,23 @@ import (
 )
 
 type testType struct {
+	name   string
 	url    string
 	expect any
 }
 
 var testCases = []testType{
 	{
-		url: "/",
+		name: "no query doesn't have query field.",
+		url:  "/",
 		expect: JsonResponse{
 			Status:  "success",
 			Message: "root handler",
 		},
 	},
 	{
-		url: "/?query=123",
+		name: "query is like int",
+		url:  "/?query=123",
 		expect: JsonResponse{
 			Status:  "success",
 			Message: "root handler",
@@ -32,7 +35,8 @@ var testCases = []testType{
 		},
 	},
 	{
-		url: "/?query=foobar",
+		name: "query is like string.",
+		url:  "/?query=foobar",
 		expect: JsonResponse{
 			Status:  "success",
 			Message: "root handler",
@@ -43,6 +47,7 @@ var testCases = []testType{
 
 var test404Cases = []testType{
 	{
+		name:   "not found status code is 404.",
 		url:    "/not-found",
 		expect: "Not Found.",
 	},
@@ -52,19 +57,21 @@ func TestHandler(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(handler))
 	t.Cleanup(func() { ts.Close() })
 
-	for _, tc := range testCases {
-		res := JsonResponse{}
-		code, b := testHelper(t, ts, tc.url)
-		if err := json.Unmarshal(b, &res); err != nil {
-			t.Errorf("error: %s", err)
-		}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			res := JsonResponse{}
+			code, b := testHelper(t, ts, tt.url)
+			if err := json.Unmarshal(b, &res); err != nil {
+				t.Errorf("error: %s", err)
+			}
 
-		if code != 200 {
-			t.Errorf("status code is not 200, but %v", code)
-		}
-		if diff := cmp.Diff(res, tc.expect); diff != "" {
-			t.Errorf("diff: %s", diff)
-		}
+			if code != 200 {
+				t.Errorf("status code is not 200, but %v", code)
+			}
+			if diff := cmp.Diff(res, tt.expect); diff != "" {
+				t.Errorf("diff: %s", diff)
+			}
+		})
 	}
 }
 
@@ -72,17 +79,19 @@ func TestNotFoundHandler(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(handler))
 	t.Cleanup(func() { ts.Close() })
 
-	for _, tc := range test404Cases {
-		var res JsonResponse
-		code, b := testHelper(t, ts, tc.url)
+	for _, tt := range test404Cases {
+		t.Run(tt.name, func(t *testing.T) {
+			var res JsonResponse
+			code, b := testHelper(t, ts, tt.url)
 
-		if code != 404 {
-			t.Errorf("status code is not 404, but %v", code)
-		}
-		if diff := cmp.Diff(string(b), tc.expect); diff != "" {
-			t.Errorf("p: %v, %v", res, tc.expect)
-			t.Errorf("diff: %s", diff)
-		}
+			if code != 404 {
+				t.Errorf("status code is not 404, but %v", code)
+			}
+			if diff := cmp.Diff(string(b), tt.expect); diff != "" {
+				t.Errorf("p: %v, %v", res, tt.expect)
+				t.Errorf("diff: %s", diff)
+			}
+		})
 	}
 }
 
