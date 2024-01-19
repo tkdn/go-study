9896925e9cf18559a983e4c82d35026ae79c4471
Satoshi TAKEDA <satoshi.takeda@classi.jp>
2024-01-20 02:01:18 +0900

    feat: schema 更新

---
+++ infra/database/post.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"time"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type postRepo struct {
+	db *sqlx.DB
+}
+
+type Post struct {
+	ID        int       `db:"id" json:"id"`
+	Text      string    `db:"text" json:"text"`
+	CreatedAt time.Time `db:"created_at" json:"createdAt"`
+}
+
